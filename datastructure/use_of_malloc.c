62ca123a59125824701fd369558d322b678f0965
prince <43395338+mcoderprince@users.noreply.github.com>
2019-09-23 00:37:04 +0530

    Create use_of_malloc.c

---
+++ datastructure/use_of_malloc.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+  int *ptr;
+  int i,n,sum=0;
+  float avg;
+  printf("Enter the size of Array:");
+  scanf("%d",&n);
+  //allocate memory for number is interger
+  ptr = (int *)malloc(n*sizeof(int));
+  printf("Enter the elements of the Array:\n");
+  for(i=0;i<n;i++){
+    printf("Enter the element:");
+    scanf("%d",ptr+i);
+  }
+//find the sum
+  for(i=0;i<n;i++){
+    sum += *(ptr+i);
+  }
+  printf("The sum is %d\n",sum);
+  avg = (sum/n);
+  printf("Average is %f",avg);
+  free(ptr);
+}
